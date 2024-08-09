eb0f3e4d7bba8bc5c3a38da5f35646abd8f36884
amersyafiq <amersyafiq1103@gmail.com>
2024-08-09 09:46:20 +0800

    Update resume

--- src/ModalPDF.js
+++ src/ModalPDF.js
@@ -8,7 +8,7 @@ const ModalPDF = ({show, modalClose}) => {
                 <BootstrapModal.Header closeButton>
                 </BootstrapModal.Header>
                 <BootstrapModal.Body>
-                    <iframe src="https://drive.google.com/file/d/1aD2Dxfsd6pYzMEpg0vtfnTPtck6WDC5L/preview" width="100%" height="100%" allow="autoplay"></iframe>
+                    <iframe src="https://drive.google.com/file/d/1rBfcxTfIkL1Q4EsDNlAlbxSz6ArkW_tF/preview" width="100%" height="100%" allow="autoplay"></iframe>
                 </BootstrapModal.Body>
                 <BootstrapModal.Footer>
                 </BootstrapModal.Footer>
